d59eac14932f1a2590e7b64014d4018d6888d605
Ron Fernandez <ronbfernandez@gmail.com>
2021-05-12 11:34:59 -0700

    adding variables for buttons, tbod, input fields, and columns

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -2,3 +2,13 @@
 var tableData = data;
 
 // YOUR CODE HERE!
+
+var tbod = d3.select("tbody");
+var button = d3.select("#filter-btn");
+var inputField1 = d3.select("#datetime");
+var inputField2 = d3.select("city");
+var reesetbtn = d3.select("#reset-btn");
+var columns = d3.select("#filter-btn");
+
+
+}
