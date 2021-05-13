03756c8297de38c75d9a302b3af8aec990b1a5d3
Ron Fernandez <ronbfernandez@gmail.com>
2021-05-12 17:49:43 -0700

    created function to populate data inout to table

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -3,12 +3,17 @@ var tableData = data;
 
 // YOUR CODE HERE!
 
-var tbod = d3.select("tbody");
+var tbody = d3.select("tbody");
 var button = d3.select("#filter-btn");
 var inputField1 = d3.select("#datetime");
 var inputField2 = d3.select("city");
-var reesetbtn = d3.select("#reset-btn");
-var columns = d3.select("#filter-btn");
+var resetbtn = d3.select("#reset-btn");
+var columns = ["datetime","city","state","country","shape","durationMinutes","comments"]
 
-
+var populate = (dataInput) => {
+    dataInput.forEach(ufo_sightings => {
+        var row = tbody.append("tr");
+        columns.forEach(column => row.append("td").text(ufo_sightings[column])
+        )
+    });
 }
